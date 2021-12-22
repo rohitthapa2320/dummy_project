d47d0d7dab482bcef40d4e52f7d70d6e6cada78e
rohitthapa2320 <rohitthapa2362000@gmail.com>
2021-12-22 08:48:18 +0530

    First Workflow

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const app=express();
 
 const PORT=3000;
 
+app.get('/close', (req,res) => {
+    res.send("Exited !!!");
+    process.exit();
+})
+
 app.get("/", (req,res) => {
-    res.send("Hello World");
+    let response="Hello World";
+    if(req.query.echop)
+        response=`${response} Param= ${req.query.echop}`;
+
+    res.send(response);
+
 });
+
 
 app.listen(PORT, () => {
     console.log(`Connected to PORT ${PORT}`);
