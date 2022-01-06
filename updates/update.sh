1ad4de1908c3f7d5d0b4ee951b569b858fe77967
rohitthapa2320 <rohitthapa2362000@gmail.com>
2022-01-06 23:44:22 +0530

    Commit

---
+++ updates/update.sh
@@ -0,0 +1,7 @@
+cd ~/dummy_server/www/dummy_project/updates/
+echo "------------ NEW RUN -------------- $(date -u)" >> processed/update.log
+
+# UPDATE SCRIPT GOES HERE
+
+# POST UPDATE CLEANUP
+mv $0 processed
