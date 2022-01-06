cfa5645b7c28cd384d8c33533133f49f488aab06
rohitthapa2320 <rohitthapa2362000@gmail.com>
2022-01-06 22:11:39 +0530

    Change made

--- action.js
+++ action.js
@@ -1,8 +1,8 @@
 const axios= require('axios');
 
-axios.get('https://testnet.binancefuture.com/fapi/v1/ticker/price?symbol=ETHBUSD')
+axios.get('https://testnet.binancefuture.com/fapi/v1/ticker/price?symbol=BTCSTUSDT')
 .then((res) => {
     console.log("Symbol Ticker Price: ", res.data);
 });
 
-// console.log(res);
+console.log("Change Made!!!");
