3e95a5fce3fe06143345a582be6654980c97b69c
rohitthapa2320 <rohitthapa2362000@gmail.com>
2022-01-08 23:01:13 +0530

    Changes Made 4

---
+++ updates/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>processed/deploy.log 2>&1
+# Everything below will go to the file 'processed/deploy.log ':
+
+cd ~/dummy_server/www/dummy_project
+echo "------------ NEW RUN -------------- $(date -u) "
+echo "  $(who)" 
+echo "  $(pwd)" 
+git pull 
+git checkout master 
+
+echo "CURL TO CLOSE" 
+curl http://localhost:3000/close
+
+echo "STAT= $(stat -c '%A' 'updates/update.sh')" 
+echo "STAT_VALUE= $([ -f "${updates/update.sh}" ] && [ -r "${updates/update.sh}" ] && [ -x "${updates/update.sh}" ])"
+if [ -f "${updates/update.sh}" ] && [ -r "${updates/update.sh}" ] && [ -x "${updates/update.sh}" ] ; then
+    echo "UPDATE SCRIPT"
+    ./updates/update.sh
+    git add .
+    git commit -m "Automatic Update"
+    git push -u
+    echo "-------------DONE WITH UPDATES ---------------"
+fi
+
+
+npm install
+node index.js &
+
--- updates/update.sh
+++ updates/update.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>processed/update.log 2>&1
+# Everything below will go to the file 'processed/update.log ':
 cd ~/dummy_server/www/dummy_project/updates/
-echo "------------ UPDATE.SH RUN -------------- $(date -u)" >> processed/update.log
+echo "------------ UPDATE.SH RUN -------------- $(date -u)"
 echo "  $(who)"
 # UPDATE SCRIPT GOES HERE !!!
 
